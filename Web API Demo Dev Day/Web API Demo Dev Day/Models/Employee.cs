c98b4d861de92f85e3fa08ffcb990dca1530d6a7
Jim Swetman <jim.swetman@milton-keynes.gov.uk>
2018-04-25 09:55:58 +0100

    Initial commit

---
+++ Web API Demo Dev Day/Web API Demo Dev Day/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_API_Demo_Dev_Day.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
